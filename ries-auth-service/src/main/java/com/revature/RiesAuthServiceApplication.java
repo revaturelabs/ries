39eb7cbbe59ffce128983cbd05bbd02fff776dd2
Andy Tang <andy.tang@nyu.edu>
2017-07-10 16:20:10 -0400

    auth init

---
+++ ries-auth-service/src/main/java/com/revature/RiesAuthServiceApplication.java
@@ -0,0 +1,12 @@
+package com.revature;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiesAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RiesAuthServiceApplication.class, args);
+	}
+}
