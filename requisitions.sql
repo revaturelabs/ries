57145b6f020480c8d105bab082f70bc6b9b5f8e0
Tyler Deans <twdeans@gmail.com>
2017-07-13 09:38:11 -0400

    updated requisitions.sql

--- requisitions.sql
+++ requisitions.sql
@@ -2,9 +2,9 @@ Create Table Requisition (
   req_ID Number(10) Primary Key,
   req_Create_Date Timestamp NOT NULL,
   req_Interview_Date Timestamp,
-  req_host Number(10) NOT NULL,
+  req_host VARCHAR2(50) NOT NULL,
   req_guest Number(10) NOT NULL,
-  req_recruiter NUMBER(10) NOT NULL,
+  req_recruiter VARCHAR2(50) NOT NULL,
   req_host_url Varchar2(2084) NOT NULL UNIQUE,
   req_guest_url Varchar2(2084) NOT NULL UNIQUE,
   req_observer_url VARCHAR2(2084) NOT NULL UNIQUE
@@ -19,7 +19,7 @@ Create Table Resolved_Requisition(
   res_req_create_date Timestamp NOT NULL,
   res_req_interview_date Timestamp,
   res_req_guest_id Number(10) NOT NULL,
-  res_req_host_id Number(10) NOT NULL,
+  res_req_host_id VARCHAR2(50) NOT NULL,
   res_req_recruiter_id NUMBER(10) NOT NULL
 );
 Create Sequence res_req_sequence
