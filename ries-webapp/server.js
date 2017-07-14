359b7d3289fe49f7fa5debbd7b646764bbd5cf29
Tyler Deans <twdeans@gmail.com>
2017-07-14 09:47:27 -0400

    updated server.js

--- ries-webapp/server.js
+++ ries-webapp/server.js
@@ -130,55 +130,6 @@ http.createServer((req, res) => {
 
 
 
-//require our websocket library 
-var WebSocketServer = require('ws').Server;
-
-//creating a websocket server at port 7000
-var wss = new WebSocketServer({ port: 7000 });
-
-//all connected to the server users 
-var users = {};
-
-//when a user connects to our sever 
-wss.on('connection', function (connection) {
-
-    console.log("User connected");
-
-    //when server gets a message from a connected user 
-    connection.on('message', function (message) {
-
-        var data;
-        //accepting only JSON messages 
-        try {
-            data = JSON.parse(message);
-        } catch (e) {
-            console.log("Invalid JSON");
-            data = {};
-        }
-
-        //switching type of the user message 
-        switch (data.type) {
-            //when a user tries to login 
-            case "login":
-                console.log("User logged", data.name);
-
-
-
-                //if anyone is logged in with this username then refuse 
-                if (users[data.name]) {
-                    sendTo(connection, {
-                        type: "login",
-                        success: false
-                    });
-                } else {
-                    //save user connection on the server 
-                    users[data.name] = connection;
-                    connection.name = data.name;
-
-                    sendTo(connection, {
-                        type: "newMember",
-                        name: data.name
-                    });
 
 //require our websocket library 
 var WebSocketServer = require('ws').Server;
