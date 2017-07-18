f7271576738e50347c2dd8861493310f4af57c74
Tyler Deans <twdeans@gmail.com>
2017-07-18 10:48:58 -0400

    added filter for requistions by host

--- ries-webapp/javascript/controllers/requisitionsCtrl.js
+++ ries-webapp/javascript/controllers/requisitionsCtrl.js
@@ -1,23 +1,23 @@
 app.controller("requisitionCtrl",['$scope', '$http', 'requisitionService', function($scope, $http, requisitionService) {
     requisitionService.getAllRequisitions().then(function(res) {
         $scope.requisitions = res;
-        console.log($scope.requisitions[0].interviewDate); // date and time are in milliseconds
+        //console.log($scope.requisitions[0].interviewDate); // date and time are in milliseconds
     });
     
+    /*
+    $scope.filterDate = function(re) {
+        console.log(re.interViewDate);
+        console.log($scope.inputDate);
+        return re.interViewDate == $scope.inputDate;
+    }*/
 }]);
 
 /*
 Filter by date or Interviewer
 */
+/*
 app.filter("interviewByDate", function() {
-    return function(date, requisitions) {
-        var filtered = [];
-
-        for (var i = 0; i < requisitions.length; i++) {
-            if (requisitions[i].interviewDate == date) {
-                filtered.push(requisitions[i]);
-            }
-        }
-        return filtered;
+    return function(date) {
+        
     }
-});
+});*/
