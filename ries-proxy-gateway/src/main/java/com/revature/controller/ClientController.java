a8e88e339d9bda22b9cda823ed7717749834af6c
Andy Tang <andy.tang@nyu.edu>
2017-07-24 11:03:06 -0400

    Changed Session Id Redirect URL

--- ries-proxy-gateway/src/main/java/com/revature/controller/ClientController.java
+++ ries-proxy-gateway/src/main/java/com/revature/controller/ClientController.java
@@ -2,8 +2,11 @@ package com.revature.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class ClientController {
@@ -11,7 +14,20 @@ public class ClientController {
     private String redirectUrl;
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String client() {
-        return "redirect:" + redirectUrl;
+    public String client(HttpSession session) {
+        String id = session.getId();
+        return "redirect:" + redirectUrl + "?JSESSIONID=" + id;
+    }
+
+    @RequestMapping(value = "/test", method = RequestMethod.GET)
+    public String testRedirect(HttpSession session) {
+        String id = session.getId();
+        return "redirect:http://localhost:3001/session?JSESSIONID=" + id;
+    }
+
+    @RequestMapping(value = "/test/{domain}/{port}", method = RequestMethod.GET)
+    public String testRedirect(HttpSession session,@PathVariable String domain, @PathVariable Integer port) {
+        String id = session.getId();
+        return "redirect:http://" + domain + ":" + port + "/session?JSESSIONID=" + id;
     }
 }
